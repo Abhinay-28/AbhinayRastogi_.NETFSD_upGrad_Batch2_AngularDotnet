1550d175d4a1d762d4674e6734d7846c8290064c
Abhinay-28 <136223036+Abhinay-28@users.noreply.github.com>
2026-03-13 08:49:53 +0530

    Add files via upload

---
+++ P1.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace hands_on_prblm_week4_day4
+{
+    class Calculator
+    {
+        // Method for addition
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Method for subtraction
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    internal class P1
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calc = new Calculator();
+
+            int addResult = calc.Add(num1, num2);
+            int subResult = calc.Subtract(num1, num2);
+
+            Console.WriteLine("Addition = " + addResult);
+            Console.WriteLine("Subtraction = " + subResult);
+
+            Console.ReadLine();
+        }
+    }
+}
